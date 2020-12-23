678ba0e6e1d93b73ba4d867b6a552f024d8ba947
Vignesh Shetty <contact@vigneshshetty.in>
2020-12-23 23:59:42 +0530

    Db

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app = Flask(__name__)
 app.secret_key = "76^)(HEY,BULK-MAILER-HERE!)(skh390880213%^*&%6h&^&69lkjw*&kjh"
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///bulkmailer.db"
 db = SQLAlchemy(app)
+
 x = datetime.now()
 time = x.strftime("%c")
 
@@ -21,17 +22,17 @@ class Groups(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     date = db.Column(db.String(50), nullable=False)
-    subscribers = db.relationship('Subscribers', backref='subscribers')
+    subscribers = db.relationship('Subscribers',cascade = "all,delete", backref='subscribers')
 
 class Subscribers(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False)
-    status = db.Column(db.Integer, nullable=False)
     date = db.Column(db.String(50), nullable=False)
     gid = db.Column(db.Integer, db.ForeignKey('groups.id'))
 
 class Organization(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(500), nullable=False)
     date = db.Column(db.String(50), nullable=False)
@@ -61,19 +62,14 @@ def register_page():
 def forgot_page():
     return render_template('forgot-password.html')
 
+
 @app.route('/view/groups')
 def group_page():
     post = Groups.query.order_by(Groups.id).all()
-    print(time)
+    # print(time)
     return render_template('group_list.html', post=post)
 
-@app.route('/view/subscribers/<string:number>', methods=['GET', 'POST'])
-def subscribers_page(number):
-    post = Subscribers.query.filter_by(gid=number).all()
-    # print(post)
-    return render_template('group_members.html', post=post)
-
-@app.route('/new/group', methods=['GET', 'POST'])
+@app.route('/new/group', methods=['POST'])
 def submit_new_group():
     if(request.method=='POST'):
         group_name = request.form.get('groupname')
@@ -82,6 +78,28 @@ def submit_new_group():
         db.session.commit()
         # flash("New group added successfully!", "success")
     return redirect('/view/groups')
+
+@app.route("/delete/group/<string:id>", methods = ['GET'])
+def delete_group(id):
+    delete_group = Groups.query.filter_by(id=id).first()
+    db.session.delete(delete_group)
+    db.session.commit()
+    # flash("Group deleted successfully!", "success")
+    return redirect('/view/groups')
+
+@app.route('/view/subscribers/<string:number>')
+def subscribers_page(number):
+    post = Subscribers.query.filter_by(gid=number).all()
+    # print(post)
+    return render_template('group_members.html', post=post)
+
+@app.route('/delete/subscriber/<string:gid>/<string:number>', methods=['GET'])
+def delete_subscriber(gid, number):
+    delete_subscriber = Subscribers.query.filter_by(id=number).first()
+    db.session.delete(delete_subscriber)
+    db.session.commit()
+    # flash("subscriber deleted successfully!", "success")
+    return redirect('/view/subscribers/'+str(gid))
 
 @app.route('/mail.html')
 def mail_page():
