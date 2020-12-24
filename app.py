51ac4f53ea09083e204ae95e589eeca52a7de68e
Vignesh Shetty <contact@vigneshshetty.in>
2020-12-24 09:58:51 +0530

    Changes

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
 
 # TODO: USE JSON TO STORE URI & OTHER IMP STUFF
 
-# import json
-#
-# with open('config.json', 'r') as c:pip
-#     json = json.load(c)["json"]
+with open('import.json', 'r') as c:
+    json = json.load(c)["jsondata"]
 
 
 app = Flask(__name__)
 app.secret_key = "76^)(HEY,BULK-MAILER-HERE!)(skh390880213%^*&%6h&^&69lkjw*&kjh"
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///bulkmailer.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = json["databaseUri"]
 db = SQLAlchemy(app)
 
 x = datetime.now()
@@ -45,24 +44,6 @@ class Organization(db.Model):
     # for post in post:
     #     elist = elist + [post.email]
     # print(elist)
-
-@app.route('/dashboard')
-def dashboard_page():
-    return render_template('index.html')
-
-@app.route('/login.html')
-def login_page():
-    return render_template('login.html')
-
-@app.route('/register.html')
-def register_page():
-    return render_template('register.html')
-
-@app.route('/forgot-password.html')
-def forgot_page():
-    return render_template('forgot-password.html')
-
-
 @app.route('/view/groups')
 def group_page():
     post = Groups.query.order_by(Groups.id).all()
@@ -114,22 +95,10 @@ def delete_subscriber(gid, number):
     # flash("subscriber deleted successfully!", "success")
     return redirect('/view/subscribers/'+str(gid))
 
-@app.route('/mail.html')
-def mail_page():
-    return render_template('mail.html')
-
-@app.route('/group_members.html')
-def group_members():
-    return render_template('group_members.html')
-
-@app.route('/user_list.html')
-def userlist_page():
-    return render_template('user_list.html')
-
-@app.errorhandler(404)
-def page_not_found(e):
-    # note that we set the 404 status explicitly
-    return render_template('404.html'), 404
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     # note that we set the 404 status explicitly
+#     return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
