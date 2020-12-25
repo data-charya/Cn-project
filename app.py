8212acf16b55c28013b10fecd7be30faecfbeffa
Vignesh Shetty <contact@vigneshshetty.in>
2020-12-25 08:28:12 +0530

    Custom 404

--- app.py
+++ app.py
@@ -379,10 +379,10 @@ def users_page():
     users = Organization.query.order_by(Organization.id).all()
     return render_template('user_list.html', users=users)
 
-# @app.errorhandler(404)
-# def page_not_found(e):
-#     # note that we set the 404 status explicitly
-#     return render_template('404.html'), 404
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
